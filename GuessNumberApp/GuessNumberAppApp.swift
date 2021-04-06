5a7b8f4bff2cb816c43c7a22112d64f0f2807cbf
Svetlana <svetaromanenko@inbox.ru>
2021-04-06 17:49:26 +0300

    Initial Commit

---
+++ GuessNumberApp/GuessNumberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberAppApp.swift
+//  GuessNumberApp
+//
+//  Created by Светлана Романенко on 06.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
